82fbcbb2a0e49a2dffc3b06f77d1f9d72079a310
Ryan Barfield <cadium828@gmail.com>
2025-08-01 10:39:32 -0500

    Phase 1, Day 1: Completed

---
+++ phase-1-fundamentals/day-1-intro-to-python/python-intro.py
@@ -0,0 +1,13 @@
+""" 
+[ ] Day 1: Introduction to Python
+• Topic: Python setup, basic syntax (variables, print statements).
+• Resource: FreeCodeCamp Python Tutorial (first 30 minutes).
+• Task: Install Python and VS Code. Write a simple program to print your name 
+and a motivational message.
+"""
+
+
+name = 'Ryan'
+motivational_message = 'you only have to be 1% better today than you were yesterday'
+
+print(f'Hello, ', name,',', motivational_message)
