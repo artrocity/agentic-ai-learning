0b76181b2bde5303d47c517461ea71c638961d6b
Ryan Barfield <cadium828@gmail.com>
2025-08-01 10:49:19 -0500

    Phase 1, Day 3 Completed

---
+++ phase-1-fundamentals/day-3-lists-and-loops/python-lists-and-loops.py
@@ -0,0 +1,14 @@
+"""
+[ ] Day 3: Lists and Loops
+• Topic: Lists, for loops, and while loops.
+• Resource: Automate the Boring Stuff with Python (Chapter 2).
+• Task: Create a list of 5 emotions and print each using a loop.
+"""
+
+# Create a list of 5 emotions
+emotions_list = ['angry', 'happy', 'sad', 'content', 'joyful']
+
+# Print each emotion in the list using a loop
+for name in emotions_list:
+  print(name)
+
